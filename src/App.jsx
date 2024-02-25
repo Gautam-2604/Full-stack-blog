08b27ab03e91d0c4c73a7329ee2fc4e7d080cf42
Gautam-2604 <gaurisariag@gmail.com>
2024-02-25 12:21:28 +0530

    loading state created and used

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import React,{useState, useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
 function App() {
   //loading state for conditional rendering
   const [loading, setLoading] = useState(true)
-  const dispatch=useDispatch()
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData){
+        dispatch(login({userData: userData}))   //.
+      }
+      else{
+        dispatch(logout)
+      }
+    })
+    .finally(()=>{setLoading(false)})
+  },[])
 
-  return (
-    <>
-      
-    </>
-  )
+  return !loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ):null
 }
 
 export default App
