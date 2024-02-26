d7bfcad0dcea2fb0dc878b562c165dd870c47d44
Gautam-2604 <gaurisariag@gmail.com>
2024-02-26 17:21:09 +0530

    react form added

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,18 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from './Header/LogoutBtn'
+import Button from './Button';
+import Input from './Input';
+import Login from "./Login";
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Login,
+    Input,
+    Button
 
 }
